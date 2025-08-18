1cd870cfa57852692a7ddac416d88cf18ef5f51c
Dharshclin24 <shavonfdclinton@gmail.com>
2025-08-18 02:52:49 -0700

    added filter function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,62 +28,54 @@ export default function Home() {
   useEffect(() => {
     getApiInfo();
   }, []); // Empty dependency array means this runs once on mount
-  filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-   x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-//   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  filterSelection("all");
+  function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    //   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+      w3RemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+    // }
+
+    // Show filtered elements
+    function w3AddClass(element, name) {
+      var i, arr1, arr2;
+      arr1 = element.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+          element.className += " " + arr2[i];
+        }
+      }
+    }
+
+    // Hide elements that are not selected
+    function w3RemoveClass(element, name) {
+      var i, arr1, arr2;
+      arr1 = element.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+          arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+      }
+      element.className = arr1.join(" ");
+    }
+
+    // // Add active class to the current control button (highlight it)
+    // var btnContainer = document.getElementById("myBtnContainer");
+    // var btns = btnContainer.getElementsByClassName("btn");
+    // for (var i = 0; i < btns.length; i++) {
+    //   btns[i].addEventListener("click", function() {
+    //     var current = document.getElementsByClassName("active");
+    //     current[0].className = current[0].className.replace(" active", "");
+    //     this.className += " active";
+    //   });
+    // }
   }
-// }
-
-// // Show filtered elements
-// function w3AddClass(element, name) {
-//   var i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++) {
-//     if (arr1.indexOf(arr2[i]) == -1) {
-//       element.className += " " + arr2[i];
-//     }
-//   }
-// }
-
-// // Hide elements that are not selected
-// function w3RemoveClass(element, name) {
-//   var i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++) {
-//     while (arr1.indexOf(arr2[i]) > -1) {
-//       arr1.splice(arr1.indexOf(arr2[i]), 1);
-//     }
-//   }
-//   element.className = arr1.join(" ");
-// }
-
-// // Add active class to the current control button (highlight it)
-// var btnContainer = document.getElementById("myBtnContainer");
-// var btns = btnContainer.getElementsByClassName("btn");
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function() {
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
-}
-
-
-
-
-
-
-
-
 
   return (
     <>
@@ -92,37 +84,35 @@ function filterSelection(c) {
         <h3>Feed Back Board</h3>
         <div className="flex-container2"></div>{" "}
         <h2>
-          Suggestions <button className="feedbackButton">Add FeedBack</button>
+          Suggestions <button className="hidden">Add FeedBack</button>
         </h2>
-      </div>
-
-      <div className="filterDiv">
-        <div id="myBtnContainer">
-          <button class="btn active" onClick="filterSelection('all')">
-            {" "}
-            All
-          </button>
-          <button class="btn" onClick="filterSelection('UI')">
-            {" "}
-            UI
-          </button>
-          <button class="btn" onClick="filterSelection('UX')">
-            {" "}
-            UX
-          </button>
-          <button class="btn" onClick="filterSelection('Enhancement')">
-            {" "}
-            Fruits
-          </button>
-          <button class="btn" onClick="filterSelection('bug')">
-            {" "}
-            Bug
-          </button>
-          <button class="btn" onClick="filterSelection('feature')">
-            Feature
-          </button>
+        <div className="filterDiv">
+          <div id="myBtnContainer">
+            <button class="btn active" onClick="filterSelection('all')">
+              {" "}
+              All
+            </button>
+            <button class="btn" onClick="filterSelection('UI')">
+              {" "}
+              UI
+            </button>
+            <button class="btn" onClick="filterSelection('UX')">
+              {" "}
+              UX
+            </button>
+            <button class="btn" onClick="filterSelection('Enhancement')">
+              {" "}
+              Fruits
+            </button>
+            <button class="btn" onClick="filterSelection('bug')">
+              {" "}
+              Bug
+            </button>
+            <button class="btn" onClick="filterSelection('feature')">
+              Feature
+            </button>
+          </div>
         </div>
-
         {/* <button>UI</button>
           <button>UX</button>
           <button>Enhancement</button>
